685822558d6daad11a35b746cb9ed943d0407281
pushpitbhardwaj <pushpit07@gmail.com>
2021-02-06 22:18:43 +0530

    Chapter 2 completed

--- Scientific_calculator.R
+++ Scientific_calculator.R
@@ -2,3 +2,30 @@
 1:5 + 6:10
 
 c(1, 3, 6, 10, 15) + c(0, 1, 3, 6, 10)
+
+sum(1:5)
+
+median(1:5)
+
+sum(1, 2, 3, 4, 5)
+
+median(1, 2, 3, 4, 5)
+
+c(2,3,5,7,11,13)-2
+
+-2:2 * -2:2
+
+identical(2 ^ 3, 2 ** 3)
+
+factorial(7) + factorial(1)
+
+choose(5, 0:5)
+# choose(k, n) choose a set of n elements from k elements
+exp(1)
+
+c("a", "b", "c", "d") < "C"
+
+x <- 1:5
+y = 6:10
+
+x+2*y-3
